b111807d06312c0520c16ef504cc0f28e552d61d
Callum B-R <cbrphys@outlook.com>
2026-08-11 22:17:31 +0100

    Add files via upload
    Define the microscope class as well as the microscope forward and backwards functions

---
+++ Physics/Microscope.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+"""
+@author: cbrph
+"""
+import numpy as np
+import matplotlib.pyplot as plt 
+plt.rcParams.update({'font.size': 40})
+from Physics import Propagation as Prg
+
+class Microscope:
+
+    def __init__(
+        self,
+        Wavelength,
+        PixelSize,
+        Distance,
+        FocalLength,
+        propagator="Fraunhofer",
+        fresnel=False,
+        lens=False
+    ):
+        self.Wavelength = Wavelength
+        self.PixelSize = PixelSize
+        self.Distance = Distance
+        self.FocalLength = FocalLength
+
+        self.propagator = propagator
+        self.fresnel = fresnel
+        self.lens = lens
+
+
+def MicroscopeForward(ExitWave, microscope):
+
+    psi = ExitWave
+
+    if microscope.propagator.lower() == "fraunhofer":
+
+        psi = Prg.FraunhoferPropagate(psi)
+
+    elif microscope.propagator.lower() == "fresnel":
+
+        psi = Prg.FresnelPropagate(
+            psi,
+            microscope.Wavelength,
+            microscope.PixelSize,
+            microscope.Distance
+        )
+
+    elif microscope.propagator.lower() == "angular_spectrum":
+
+        psi = Prg.AngularSpectrumPropagate(
+            psi,
+            microscope.Wavelength,
+            microscope.PixelSize,
+            microscope.Distance
+        )
+
+    else:
+        raise ValueError(
+            f"Unknown propagator: {microscope.propagator}"
+        )
+
+    return psi
+
+
+def MicroscopeBackward(DetectorWave, microscope):
+
+    psi = DetectorWave
+
+    if microscope.propagator.lower() == "fraunhofer":
+
+        psi = Prg.BackFraunhoferPropagate(psi)
+
+    elif microscope.propagator.lower() == "fresnel":
+
+        psi = Prg.BackFresnelPropagate(
+            psi,
+            microscope.Wavelength,
+            microscope.PixelSize,
+            microscope.Distance
+        )
+
+    elif microscope.propagator.lower() == "angular_spectrum":
+
+        psi = Prg.BackAngularSpectrumPropagate(
+            psi,
+            microscope.Wavelength,
+            microscope.PixelSize,
+            microscope.Distance
+        )
+
+    else:
+        raise ValueError(
+            f"Unknown propagator: {microscope.propagator}"
+        )
+
+    return psi
